0dff6fe71ebb92eb19436108ddef84b0b51dd0d6
SidV2 <sidharthv91@gmail.com>
2022-05-02 11:27:56 -0400

    Update code.ts

--- src/code.ts
+++ src/code.ts
@@ -2,58 +2,58 @@ import { Observable, Subject } from 'rxjs';
 import { map, share } from 'rxjs/operators';
 import { fromEvent } from 'rxjs';
 
-//emit values over time, basically a stream concept
-// let observable = new Observable((observer) => {
-//     try {
-//         observer.next('Hey guys');
-//         observer.next('How are you?');
-//         setInterval(() => {
-//             observer.next('I am good');
-//         }, 2000);
-//     }
-//     catch (err) {
-//         observer.error();
-//     }
-// }).pipe(share()); //pipe will chain operators
+emit values over time, basically a stream concept
+let observable = new Observable((observer) => {
+    try {
+        observer.next('Hey guys');
+        observer.next('How are you?');
+        setInterval(() => {
+            observer.next('I am good');
+        }, 2000);
+    }
+    catch (err) {
+        observer.error();
+    }
+}).pipe(share()); //pipe will chain operators
 
-//to get value you have to subscribe to the above observable
-// let observer = observable.subscribe({
-//     next(x) { console.log('got value ' + x); },
-//     error(err) { console.error('something wrong occurred: ' + err); },
-//     complete() { console.log('done'); }
-// });
+to get value you have to subscribe to the above observable
+let observer = observable.subscribe({
+    next(x) { console.log('got value ' + x); },
+    error(err) { console.error('something wrong occurred: ' + err); },
+    complete() { console.log('done'); }
+});
 
-//second observer
-// let observer2 = observable.subscribe({
-//     next(x) { console.log('got value ' + x); },
-//     error(err) { console.error('something wrong occurred: ' + err); },
-//     complete() { console.log('done'); }
-// });
+second observer
+let observer2 = observable.subscribe({
+    next(x) { console.log('got value ' + x); },
+    error(err) { console.error('something wrong occurred: ' + err); },
+    complete() { console.log('done'); }
+});
 
-//observer 2 added to observer so now they both will be unsubscribed in the settimeout func below
-//observer.add(observer2);
+observer 2 added to observer so now they both will be unsubscribed in the settimeout func below
+observer.add(observer2);
 
-//cancel subscription to not receive values anymore
-// setTimeout(() => {
-//     observer.unsubscribe();
-// }, 6001);
+cancel subscription to not receive values anymore
+setTimeout(() => {
+    observer.unsubscribe();
+}, 6001);
 
-//hot observable
-// let observableMouseEvent = fromEvent(document, 'mousemove');
-// setTimeout(() => {
-//     observableMouseEvent.subscribe(observer => {
-//         console.log(observer);
-//     });
-// }, 2000);
+hot observable
+let observableMouseEvent = fromEvent(document, 'mousemove');
+setTimeout(() => {
+    observableMouseEvent.subscribe(observer => {
+        console.log(observer);
+    });
+}, 2000);
 
-//create a Subject a special type of observer and also an observable
-// let subject = new Subject();
-// subject.subscribe(data => {
-//     console.log(data);
-// });
+create a Subject a special type of observer and also an observable
+let subject = new Subject();
+subject.subscribe(data => {
+    console.log(data);
+});
 
-// //send or emit as an observable
-// subject.next("Subject item sent");
+//send or emit as an observable
+subject.next("Subject item sent");
 
 //operator map example
 const observable3 = new Observable((observer: any) => {
@@ -83,3 +83,13 @@ subject.subscribe((data) => console.log('Subscriber A', data));
 subject.subscribe((data) => console.log('Subscriber B', data));
 subject.next(Math.random());
 subject.next(Math.random());
+
+//Deep Copy objects in JS. Can be used for reducers in ngRX
+let a = {
+    name: 'Sid,
+    address: {
+        city: 'Windsor'
+    }
+}
+let adress = {...a.address, city: 'London'};
+let modifiedState = {...a, name: 'Sid1', address: adress};
