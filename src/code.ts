1b8d1d0e5bfffcdc8303f1acce07fac459d43a11
SidV2 <sidharthv91@gmail.com>
2024-04-10 11:11:15 -0300

    Update code.ts

--- src/code.ts
+++ src/code.ts
@@ -2,7 +2,7 @@ import { Observable, Subject } from 'rxjs';
 import { map, share } from 'rxjs/operators';
 import { fromEvent } from 'rxjs';
 
-emit values over time, basically a stream concept
+//emit values over time, basically a stream concept
 let observable = new Observable((observer) => {
     try {
         observer.next('Hey guys');
@@ -16,29 +16,29 @@ let observable = new Observable((observer) => {
     }
 }).pipe(share()); //pipe will chain operators
 
-to get value you have to subscribe to the above observable
+//to get value you have to subscribe to the above observable
 let observer = observable.subscribe({
     next(x) { console.log('got value ' + x); },
     error(err) { console.error('something wrong occurred: ' + err); },
     complete() { console.log('done'); }
 });
 
-second observer
+//second observer
 let observer2 = observable.subscribe({
     next(x) { console.log('got value ' + x); },
     error(err) { console.error('something wrong occurred: ' + err); },
     complete() { console.log('done'); }
 });
 
-observer 2 added to observer so now they both will be unsubscribed in the settimeout func below
+observer 2 //added to observer so now they both will be unsubscribed in the settimeout func below
 observer.add(observer2);
 
-cancel subscription to not receive values anymore
+//cancel subscription to not receive values anymore
 setTimeout(() => {
     observer.unsubscribe();
 }, 6001);
 
-hot observable
+//hot observable
 let observableMouseEvent = fromEvent(document, 'mousemove');
 setTimeout(() => {
     observableMouseEvent.subscribe(observer => {
@@ -59,30 +59,6 @@ subject.next("Subject item sent");
 const observable3 = new Observable((observer: any) => {
     observer.next("hello");
 }).pipe(map(data => data.toString().toUpperCase())).subscribe(data => console.log(data));
-
-
-import { Observable, Subject } from 'rxjs';
-
-import { Observable, Subject } from 'rxjs';
-
-const subject = new Subject<number>();
-
-//Cold observables
-const observable1 = new Observable((observer) => {
-  observer.next(Math.random()); //lazy will only emit when subscribed
-});
-
-const observable2 = new Observable((observer) => {
-  observer.next(Math.random()); //lazy will only emit when subscribed
-});
-observable1.subscribe((data) => console.log(data));
-observable2.subscribe((data) => console.log(data));
-
-//Hot observables
-subject.subscribe((data) => console.log('Subscriber A', data));
-subject.subscribe((data) => console.log('Subscriber B', data));
-subject.next(Math.random());
-subject.next(Math.random());
 
 //Deep Copy objects in JS. Can be used for reducers in ngRX
 let a = {
@@ -116,3 +92,42 @@ const b = {
         }
     }
 }
+
+
+//Topic of Hot vs Cold Observables in Angular
+//Cold observables
+const cold$ = of(null).pipe(map(() => Math.random()));
+
+//different number
+const a = cold$.subscribe((data) => console.log(data));
+
+//different number
+const b = cold$.subscribe((data) => console.log(data));
+
+//Hot observables
+const hot$ = of(null).pipe(
+  map(() => Math.random()),
+  shareReplay()
+);
+
+//Same input for both these 2
+//Multicasted
+const c = hot$.subscribe((data) => console.log(data));
+const d = hot$.subscribe((data) => console.log(data));
+
+//What if one is subscribed before and one is subscribed after the observer emits
+const subject$ = new Subject(); // Create a Subject
+
+setTimeout(() => {
+  subject$.next(Math.random());
+}, 3000);
+
+// Subscribe Observer A
+setTimeout(() => {
+  subject$.subscribe((value) => console.log(`observer A: ${value}`));
+}, 2100);
+
+// Subscribe Observer B
+setTimeout(() => {
+  subject$.subscribe((value) => console.log(`observer B: ${value}`));
+}, 4100);
